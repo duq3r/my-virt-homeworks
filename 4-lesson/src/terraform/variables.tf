7b6d8b3ad8884d0c71b16a7ef90c760097cee562
duq3r <duker.ak@gmail.com>
2022-08-31 11:44:46 +0300

    4-lesson-src

---
+++ 4-lesson/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1geim0buhe7h6mvpbtr"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gc0if4fp4krujvnc5u"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8p0hvmp71isp7hd1ar"
+}
