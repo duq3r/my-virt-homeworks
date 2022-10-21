ee9c393d6202830e548df44d5cf920f00bfb854c
“Evgheni <e.madan@znanierussia.ru>
2022-10-21 17:33:13 +0300

    fixes

---
+++ 7-02-terraform-1/src/providers.tf
@@ -0,0 +1,15 @@
+# Provider
+provider "yandex" {
+  token     = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  zone      = var.yc_region
+}
+
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.61.0"
+    }
+  }
+}
