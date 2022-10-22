2e9fd3426b9990bcf2b21e9c3e1f73f843f1eb34
“Evgheni <e.madan@znanierussia.ru>
2022-10-22 14:34:46 +0300

    fix

--- 7-03-Terraform/src1/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "access_key" {
-  value = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  sensitive = true
-}
-output "secret_key" {
-  value = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  sensitive = true
-}
